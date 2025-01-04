6b2d39aa341ad297ff1ad4be0deba39f969a2dfe
Thulani Tyaphile <tj.tyaphile@gmail.com>
2025-01-04 20:37:21 +0200

    Prepare deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,4 @@
-import {useEffect, useState} from "react";
-import client from "../utils/connect.ts";
-import Spinner from "../components/Spinner.tsx";
-import BasicTable from "../components/BasicTable.tsx";
-import Button from "../components/Button.tsx";
-
 export default function Home() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState([]);
 
     // useEffect(() => {
     //    client("").get("/users/alpha-thulz/repos")
@@ -23,13 +15,13 @@ export default function Home() {
     return (
         <div className="container">
             {
-                isLoading ?
-                <Spinner /> :
-                (
+                // isLoading ?
+                // <Spinner /> :
+                // (
                     <div className="container-fluid">
                         <h1><center>Tenants Front End</center></h1>
                     </div>
-                )
+                // )
             }
         </div>
     );
